a0dcbff993b242dee45e963ee6ccdd55d527f6d6
Rovak0 <ericjackwong@outlook.com>
2024-07-11 17:41:33 -0400

    Not triangle shapes

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 //needs to prompt for text, read text, reference the shapes, make the item, save the item
 const inquirer = require('inquirer');
+const circle = require('./lib/circle');
+const square = require('./lib/square');
+
+
 
 const questions = ['What 3 letters would you like in your logo?', 'What color would you like for the text color?', 
     'What shape would you like? (Square, Triangle, or Circle', 'What color would you like the shape?'
 ];
+
+
+
+//lets have the logo maker be its own function
+function svgGenerator(queryData) {
+    //text, textColor,shape, shapeColor
+    //not worried about text stuff
+    const shape = queryData.shape;
+    const shapeColor = queryData.shapeColor;
+    switch(shape){
+        case 'circle':
+            return circle(shapeColor);
+        case 'square':
+            return square(shapeColor);
+        case 'triangle':
+            return triangle(shapeColor);
+        default:
+            break;
+    }
+}
+
 
 //set up the query
 inquirer
@@ -32,4 +57,5 @@ inquirer
     ])
     .then(function(response){
         console.log(response);
+        console.log(svgGenerator(response));
     })
