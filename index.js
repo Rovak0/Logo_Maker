bc03df59d0bb98f38b1fe0a57f7ab9f4c725c134
Rovak0 <ericjackwong@outlook.com>
2024-07-11 19:39:04 -0400

    Making the logo

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 //needs to prompt for text, read text, reference the shapes, make the item, save the item
 const inquirer = require('inquirer');
+const fs = require('fs');
 const circle = require('./lib/circle');
 const square = require('./lib/square');
+const triangle = require('./lib/triangle');
 
 
-
+//array of questions to be asked
 const questions = ['What 3 letters would you like in your logo?', 'What color would you like for the text color?', 
     'What shape would you like? (Square, Triangle, or Circle', 'What color would you like the shape?'
 ];
 
-
+//the write function
+function writeSvg(text) {
+    fs.writeFile('logo.html', text, (err) => {
+        err ? console.log(err) : console.log("working")
+    })
+}
 
 //lets have the logo maker be its own function
 function svgGenerator(queryData) {
@@ -57,5 +64,14 @@ inquirer
     ])
     .then(function(response){
         console.log(response);
-        console.log(svgGenerator(response));
+        const logoShape = svgGenerator(response);
+        console.log(logoShape);
+        //make the text components
+        const logoText = `<text x=30 y=55 fill="${response.textColor}">${response.text}"</text>`;
+        const svgLogo = `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+        ${logoShape}
+        ${logoText}
+        </svg>`;
+        writeSvg(svgLogo);
+
     })
